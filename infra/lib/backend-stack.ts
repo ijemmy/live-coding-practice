d6c5468998c6b0949a78396797bc98350f819d02
ijemmy <ijemmy@users.noreply.github.com>
2021-04-06 16:28:31 +0200

    Get Lambda + DynamoDB in

--- infra/lib/backend-stack.ts
+++ infra/lib/backend-stack.ts
@@ -1,41 +1,19 @@
-import * as cdk from '@aws-cdk/core';
-import * as awsAPIGatewayDynamoDB from '@aws-solutions-constructs/aws-apigateway-dynamodb';
-import * as apigateway from '@aws-cdk/aws-apigateway';
-export interface BackendStackProps extends cdk.StackProps {
-
-}
+import * as cdk from "@aws-cdk/core";
+import * as lambdaDynamoDB from "@aws-solutions-constructs/aws-lambda-dynamodb";
+import * as lambda from "@aws-cdk/aws-lambda";
+export interface BackendStackProps extends cdk.StackProps {}
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: BackendStackProps) {
     super(scope, id, props);
 
-    const construct = new awsAPIGatewayDynamoDB.ApiGatewayToDynamoDB(this, 'FeedbackAPI', {
-        allowCreateOperation: true,
-        allowReadOperation: true,
-        // allowUpdateOperation: true, 
-        // allowDeleteOperation: true,
-        createRequestTemplate: JSON.stringify({
-          TableName: '${Table}',
-          Item: {
-            'Key': { S: "$input.path('$.key')" },
-            'Name': { S: "$input.path('$.name')" },
-            'Email': { S: "$input.path('$.email')" },
-            'Subject': { S: "$input.path('$.subject')" },
-            'Details': { S: "$input.path('$.details')" },
-          }
-        })
+    new lambdaDynamoDB.LambdaToDynamoDB(this, "test-lambda-dynamodb-stack", {
+      lambdaFunctionProps: {
+        code: lambda.Code.fromAsset(
+          `${__dirname}/feedback-service-lambdas`
+        ),
+        runtime: lambda.Runtime.NODEJS_12_X,
+        handler: "index.handler",
+      },
     });
-
-    construct.apiGateway.methods.forEach((apiMethod) => {
-      const child = apiMethod.node.findChild("Resource") as apigateway.CfnMethod;
-        child.addPropertyOverride("AuthorizationType", "NONE");
-    });
-
-
-    
-    // const root = construct.apiGateway.root;
-    // root.addResource('feedback').addMethod('POST');
-
-    // How do we link this to config stack
   }
-
 }
