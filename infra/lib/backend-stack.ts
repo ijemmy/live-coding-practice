1400b5a56ed1e2d4c98e0646d0a2d30358413964
ijemmy <ijemmy@users.noreply.github.com>
2021-04-06 16:39:25 +0200

    Get handler to work

--- infra/lib/backend-stack.ts
+++ infra/lib/backend-stack.ts
@@ -1,19 +1,27 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambdaDynamoDB from "@aws-solutions-constructs/aws-lambda-dynamodb";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigateway from "@aws-cdk/aws-apigateway";
+
 export interface BackendStackProps extends cdk.StackProps {}
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: BackendStackProps) {
     super(scope, id, props);
 
-    new lambdaDynamoDB.LambdaToDynamoDB(this, "test-lambda-dynamodb-stack", {
+    const lambdaToDynamoDB = new lambdaDynamoDB.LambdaToDynamoDB(this, "test-lambda-dynamodb-stack", {
       lambdaFunctionProps: {
-        code: lambda.Code.fromAsset(
-          `${__dirname}/feedback-service-lambdas`
-        ),
+        code: lambda.Code.fromAsset(`${__dirname}/feedback-service-lambdas`),
         runtime: lambda.Runtime.NODEJS_12_X,
         handler: "index.handler",
       },
     });
+
+    const api = new apigateway.RestApi(this, "feedback-api", {});
+    const integration = new apigateway.LambdaIntegration(lambdaToDynamoDB.lambdaFunction);
+
+
+    const feedbacks = api.root.addResource("feedbacks");
+    feedbacks.addMethod('POST', integration);
+    
   }
 }
